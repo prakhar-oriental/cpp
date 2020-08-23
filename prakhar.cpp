df5ee8cc02b779327a45c088ab50eaa700f5a798
prakhar-oriental <62442016+prakhar-oriental@users.noreply.github.com>
2020-08-23 23:03:20 +0530

    Add files via upload

---
+++ prakhar.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+ class test
+{
+
+    float cg;
+    char grade;
+    public:
+    test(float x,char y)
+    {
+        cg=x;
+        grade=y;
+
+        cout<<"cg="<<x<<endl<<"grade="<<y<<endl;
+    }
+    test()
+
+    {
+
+    }       cg=x;
+        grade=y;
+     cout<<"cg="<<x<<endl<<"grade="<<y<<endl;
+    }
+};
+int main()
+{
+    test t(8.8,'A');
+    test t;
+    return 0;
+}
+
+
